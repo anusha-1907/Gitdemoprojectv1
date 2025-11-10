07dc0944bbff772886817a86728ca7f07c24d937
anusha-1907 <anushasagadabona@gmail.com>
2025-11-10 14:59:03 +0530

    commiting test2

---
+++ src/test/Test2.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eating something1");
+		else
+		{
+			System.out.println("eating nothing");
+		}
+		
+	}
+
+}
