79cd4e31b997ed8e15e3179e89afd3b30afdf61d
anusha-1907 <anushasagadabona@gmail.com>
2025-11-10 14:34:35 +0530

    first commit

---
+++ src/test/Test1.java
@@ -0,0 +1,17 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) 
+	{
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eating something");
+		else
+		{
+			System.out.println("eating nothing");
+		}
+		
+	}
+
+}
