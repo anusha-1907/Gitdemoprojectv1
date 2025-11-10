10116c33ccee103109118fcb4284dfd9253f0cee
anusha-1907 <anushasagadabona@gmail.com>
2025-11-10 15:18:37 +0530

    added addition

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -5,7 +5,7 @@ public class Test2 {
 	public static void main(String[] args) 
 	{
 		String hungry = "yes";
-		if(hungry == "yes")
+		if(hungry == "yes"|| hungry =="y")
 			System.out.println("eating something1");
 		else
 		{
